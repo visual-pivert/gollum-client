ae5b15910a51349fdeea7713c3e4923706b1d2f5
Manamahefa <manamahefa.nyr@gmail.com>
2024-06-12 11:52:16 +0300

    feat: init prodConfig

---
+++ src/renderer/src/components/prod_config/ProdConfig.svelte
@@ -0,0 +1,14 @@
+<script>
+	let tech_list = ['wordpress', 'laravel', 'symfony', 'django']
+</script>
+
+<div class="bg-background2 p-3 rounded-lg">
+	<div>
+		<label for="tech">technologie:</label>
+		<select name="tech" id="tech">
+			{#each tech_list as tech}
+				<option value={tech}>{tech}</option>
+			{/each}
+		</select>
+	</div>
+</div>
