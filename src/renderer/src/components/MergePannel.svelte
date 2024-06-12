99e4b36f46fa75d29063f1cc0b8100427409fbee
Manamahefa <manamahefa.nyr@gmail.com>
2024-06-12 13:33:08 +0300

    feat: ajout de recherche dans mergePannel

--- src/renderer/src/components/MergePannel.svelte
+++ src/renderer/src/components/MergePannel.svelte
@@ -13,6 +13,16 @@
 		{ branch_name: 'module' }
 	]
 
+	let to_search = ''
+	let search_focus = false
+
+	$: result = branch_list.reduce((acc, branch) => {
+		if (branch.branch_name.toLowerCase().startsWith(to_search.toLowerCase())) {
+			acc = [...acc, branch]
+		}
+		return acc
+	}, [])
+
 	let checked_branch = null
 
 	function merge(current_branch, to_merge) {
@@ -22,10 +32,31 @@
 
 <div class="flex flex-col gap-2 p-3 h-full">
 	<span>Merge avec:</span>
+	<div
+		class="h-7
+			{search_focus ? 'border-2 border-blue-btn' : 'border border-background3'}
+			border-solid rounded px-2"
+	>
+		<label for="search-branch" class="flex gap-1 items-center">
+			<i class="ri-search-line"></i>
+			<input
+				id="search-branch"
+				type="text"
+				bind:value={to_search}
+				class="bg-transparent p-1 outline-none flex-grow"
+				on:focus={() => {
+					search_focus = true
+				}}
+				on:blur={() => {
+					search_focus = false
+				}}
+			/>
+		</label>
+	</div>
 	<div class="branch-container flex-grow overflow-auto">
 		<ul class="ml-2">
-			{#each branch_list as branch}
-				<li class="flex gap-2 items-center p-1">
+			{#each result as branch}
+				<li>
 					<input
 						type="radio"
 						name="merge"
@@ -36,12 +67,17 @@
 						}}
 						class="hidden"
 					/>
-					{#if branch == checked_branch}
-						<i class="ri-checkbox-circle-fill text-blue"></i>
-					{:else}
-						<i class="ri-checkbox-blank-circle-line text-font-color2"></i>
-					{/if}
-					<label for={branch.branch_name}>{branch.branch_name}</label>
+					<label
+						for={branch.branch_name}
+						class="cursor-pointer flex gap-2 items-center p-1 px-2 rounded hover:bg-background4 opacity-80
+							{branch == checked_branch ? 'bg-background4 opacity-100' : ''}"
+					>
+						<i
+							class="ri-git-branch-line
+								{branch == checked_branch ? 'text-blue' : 'text-font-color'}"
+						></i>
+						{branch.branch_name}
+					</label>
 				</li>
 			{/each}
 		</ul>
@@ -50,7 +86,7 @@
 		<Buttons
 			label="Merge"
 			bg_color="--blue-btn"
-			on:click={() => merge($selected_branch, checked_branch)}
+			on:click={() => merge($selected_branch, checked_branch.branch_name)}
 			disabled={!checked_branch}
 		/>
 	</div>
